2365109969616261b4ba951a6a129750f8714fa3
doubowen <2313324267@qq.com>
2019-01-08 09:45:23 +0800

    首次上传到公网github

---
+++ home_application/urls.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import patterns
+
+urlpatterns = patterns(
+    'home_application.views',
+    (r'^$', 'home'),
+    (r'^test/$', 'test'),
+    (r'^api/get_hosts/$', 'get_hosts'),
+    (r'^api/fast_execute_script/$', 'fast_execute_script'),
+    (r'^history/$', 'history'),
+    (r'^home/$', 'home'),
+)
